a47f5e433fbef53c22d78ca5f14fdf56f29cceb7
dylanhusand <58237048+dylanhusand@users.noreply.github.com>
2021-09-21 12:55:40 +0700

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,27 @@
+const chalk = require('chalk');
+const rpc = require("discordrpcgenerator");
+const config = require(".././config.json")
+
+module.exports = async(client) => {
+  const small = await rpc.getRpcImage(config.applicationid, config.smallimgname);
+  
+  rpc.getRpcImage(config.applicationid, config.imagename)
+  .then(large => {
+    let presence = new rpc.Rpc()
+    .setName(config.name)
+    .setUrl('https://www.twitch.tv/ga')
+    .setType(config.type)
+    .setApplicationId(config.applicationid)
+    .setAssetsLargeImage(large.id)
+
+.setAssetsSmallImage(small.id)   
+      .setAssetsLargeText(large.name)
+    .setState(config.state)
+    .setDetails(config.details)
+    .setStartTimestamp(config.time || Date.now())
+    console.log(presence.toDiscord())
+    client.user.setStatus("online")
+    client.user.setPresence(presence.toDiscord()).catch(console.error);
+  })
+  console.log(chalk.hex("#800080")("Succesfully enabled rpc"))
+                  }
